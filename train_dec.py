2e653f9468e677d570bec8160c3fac29cec53482
uthree <uthree271828@gmail.com>
2024-01-25 20:22:04 +0900

    change weight

--- train_dec.py
+++ train_dec.py
@@ -37,8 +37,8 @@ parser.add_argument('-no-spk', default=False, type=bool)
 
 parser.add_argument('--weight-adv', default=2.5, type=float)
 parser.add_argument('--weight-feat', default=1.0, type=float)
-parser.add_argument('--weight-mel', default=1.0, type=float)
-parser.add_argument('--weight-con', default=20.0, type=float)
+parser.add_argument('--weight-mel', default=10.0, type=float)
+parser.add_argument('--weight-con', default=10.0, type=float)
 
 args = parser.parse_args()
 
