e34a36ecde27276520a6bf5f750e10a1694ce375
uthree <uthree271828@gmail.com>
2024-01-27 09:58:54 +0900

    change default length

--- train_dec.py
+++ train_dec.py
@@ -28,7 +28,7 @@ parser.add_argument('-lr', '--learning-rate', type=float, default=1e-4)
 parser.add_argument('-d', '--device', default='cuda')
 parser.add_argument('-e', '--epoch', default=1000, type=int)
 parser.add_argument('-b', '--batch-size', default=16, type=int)
-parser.add_argument('-len', '--length', default=64000, type=int)
+parser.add_argument('-len', '--length', default=32000, type=int)
 parser.add_argument('-m', '--max-data', default=-1, type=int)
 parser.add_argument('-fp16', default=False, type=bool)
 
