a62f12a39b37fab50997fa8192db487af512bba3
uthree <uthree271828@gmail.com>
2024-01-31 19:42:31 +0900

    mel weight is 5.0 is best. maybe.

--- train_dec.py
+++ train_dec.py
@@ -34,7 +34,7 @@ parser.add_argument('-fp16', default=False, type=bool)
 parser.add_argument('--disc-interval', default=1, type=int)
 
 parser.add_argument('--weight-stft', default=1.0, type=float)
-parser.add_argument('--weight-mel', default=1.0, type=float)
+parser.add_argument('--weight-mel', default=5.0, type=float)
 
 args = parser.parse_args()
 
