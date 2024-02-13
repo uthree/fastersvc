f0fafc3c922801e156f40351f25d9a6b76e409db
uthree <uthree271828@gmail.com>
2024-02-13 17:36:33 +0900

    improve training

--- train_dec.py
+++ train_dec.py
@@ -35,7 +35,7 @@ parser.add_argument('--disc-interval', default=1, type=int)
 
 parser.add_argument('--weight-stft', default=1.0, type=float)
 parser.add_argument('--weight-adv', default=1.0, type=float)
-parser.add_argument('--weight-mel', default=45, type=float)
+parser.add_argument('--weight-mel', default=1.0, type=float)
 
 args = parser.parse_args()
 
